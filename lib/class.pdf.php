e5828b1d1745a14b64eb3d5def3a696537e511ee
gloton <gloton@gmail.com>
2013-01-02 15:21:03 -0300

    html a pdf pagare OK
    esta el html del pagare, este es convertido a pdf, con codificacion
    utf8, aunque  ahora faltaria rellenar los datos dinamicamente.

--- lib/class.pdf.php
+++ lib/class.pdf.php
@@ -2077,7 +2077,7 @@ class  Cpdf {
 
     $xref = array();
 
-    $content = "%PDF-1.3\n%âãÏÓ\n";
+    $content = "%PDF-1.3\n%ï¿½ï¿½ï¿½ï¿½\n";
 
     //  $content="%PDF-1.3\n";
     $pos = strlen($content);
@@ -4329,7 +4329,8 @@ class  Cpdf {
 
     $tmp =  get_magic_quotes_runtime();
 
-    set_magic_quotes_runtime(0);
+    //set_magic_quotes_runtime(0);
+    ini_set("magic_quotes_runtime", 0);
 
     if  ( ($data =  file_get_contents($file)) ===  false) {
 
@@ -4345,7 +4346,9 @@ class  Cpdf {
       $errormsg =  'trouble opening file: '.$file;
     }
 
-    set_magic_quotes_runtime($tmp);
+    //set_magic_quotes_runtime($tmp);
+    ini_set("magic_quotes_runtime", $tmp);
+    
 
 
     if  (!$error) {
---
+++ pagarehtml.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+  <style>
+    @page { margin: 0px 50px; }
+    #header { position: fixed; left: 0px; top: -180px; right: 0px; height: 150px; background-color: orange; text-align: center; }
+    #footer { position: fixed; left: 0px; bottom: -180px; right: 0px; height: 150px; background-color: lightblue; }
+    #footer .page:after { content: counter(page, upper-roman); }
+  </style>
+<body>
+	<div id="content">
+	  	<h1 style="text-align: center;">PAGARÉ</h1>
+		<p>1) Debo y pagaré incondicionalmente a la orden de COMERCIAL BIENVENIDO SPA, 
+		representada legalmente por don Juan José Tarrasón Marchant, ambos 
+		domiciliados en calle Fontt número doscientos sesenta bis, de la Comuna de 
+		Colina, la cantidad de  $ _________PESOS.
+		</p>
+		<p>2) El capital adeudado se pagará en el domicilio del deudor, dentro del 
+		plazo DE _____ días, con vencimiento al día ____________________
+		</p>
+		<p>3) No obstante el plazo del pagaré es de__________días, las partes de común 
+		acuerdo podrán anticipar el vencimiento.
+		</p>
+		<p>4) El deudor tendrá derecho a prepagar total o parcialmente la obligación 
+		en cualquier momento.
+		</p>
+		<p>5) La mora o el simple retardo en el pago de la cantidad adeudada al 
+		vencimiento del plazo, facultará a COMERCIAL BIENVENIDO SPA, representada legalmente 
+		por don Juan José Tarrasón Marchant,  para exigir en forma inmediata y sin 
+		obligación de protesto, el total de la suma adeudada, y a cobrar como interés 
+		penal, el máximo convencional que la ley permite en operaciones no reajustables, 
+		interés que se aplicará sobre el total del saldo adeudado, desde la mora o simple 
+		retardo y hasta la fecha de pago efectivo.
+		</p>
+		<p>6) La obligación derivada del presente pagaré, se considerará indivisible para 
+		todos los efectos legales.
+		</p>
+		<p>7) Todos los derechos, impuestos, gastos notariales, honorarios u otros desembolsos 
+		que afecten o puedan afectar a este documento con motivo de su suscripción, protesto, 
+		cobranza judicial o extrajudicial, serán de cargo del suscriptor del pagaré.
+		</p>
+		<p>8) Para todos los efectos legales derivados del presente pagaré las partes fijan 
+		su domicilio en la comuna de RANCAGUA, y se someten a la Jurisdicción de los 
+		Tribunales de Justicia de  Rancagua, República de Chile.
+		</p>
+		<p style="margin-top: 80px; margin-bottom: 40px;">Santiago, _____de ______________de 20____
+		</p>
+		<p style="text-align: center;">__________________________________________________ <br/>Suscriptor (nombre completo, 
+		rut y firma)
+		</p>
+		<p style="text-align: center;">__________________________________________________ <br/>Dirección y teléfono
+		</p>
+	</div>
+</body>
+</html>
